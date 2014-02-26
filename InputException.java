6d0eed6562d1eac93a8e5787849e39b442d22e07
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-02-26 14:48:35 +0100

    AddressParser InputException

---
+++ InputException.java
@@ -0,0 +1,20 @@
+package mapofdenmark;
+
+/**
+ *
+ * @author Sjurdur
+ */
+class InputException extends Exception
+{
+
+    public InputException()
+    {
+        
+    }
+    
+    public InputException(String message)
+    {
+        super(message);
+    }
+    
+}
