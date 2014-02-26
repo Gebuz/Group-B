d248bda7cf09219052f40525bcbc01634b86fd56
Johan Neergaard <johanln@hotmail.dk>
2014-02-26 13:20:27 +0100

    update

--- InputException.java
+++ InputException.java
@@ -2,23 +2,19 @@ package addressparser;
 
 /**
  *
- * @author Johann
+ * @author Sjurdur
  */
+class InputException extends Exception
+{
 
+    public InputException()
+    {
+        
     }
+    
+    public InputException(String message)
+    {
         super(message);
     }
-
-    public InputException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InputException(Throwable cause) {
-        super(cause);
-    }
-
-    public InputException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
-
+    
 }
