1709e82881e1647c1e67faa0a8a10437645d0de7
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-02-26 13:29:57 +0100

    Tilføjet ny klasse

---
+++ TestNewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapofdenmark;
+
+/**
+ *
+ * @author Sjurdur
+ */
+public class TestNewClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Jååå rulåår");
+    }
+}
